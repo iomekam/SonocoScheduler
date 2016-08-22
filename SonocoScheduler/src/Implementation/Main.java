e47fa0a5f7564c6b56d5ebb7cd1e64748d0c736a
rleonard3 <duce332@yahoo.com>
2016-08-22 14:59:21 -0400

    Added Scheduler class

--- SonocoScheduler/src/Implementation/Main.java
+++ SonocoScheduler/src/Implementation/Main.java
@@ -1,7 +1,7 @@
 package Implementation;
 
 import interfaces.IComponentTimer;
-import mocks.MockScheduler;
+
 
 public class Main {
 
@@ -9,16 +9,16 @@ public class Main {
 		InstanceFactory factory = InstanceFactory.get();
 		
 		factory.InitializePress(new Press(1, 90, 30, 200,1));
-		factory.InitializePress(new Press(2, 240, 60, 215,2));
+		factory.InitializePress(new Press(2, 240, 60, 200,2));
 		factory.InitializePress(new Press(3, 150, 22, 200,3));
-		factory.InitializePress(new Press(4, 90, 34, 215,4));
+		factory.InitializePress(new Press(4, 90, 34, 200,4));
 		factory.InitializePress(new Press(5, 130, 24, 200,5));
-		factory.InitializePress(new Press(6, 90, 34, 215,6));
+		factory.InitializePress(new Press(6, 90, 34, 200,6));
 		
 		factory.InitializeExtuder(new Extruder(1));
 		factory.InitializeExtuder(new Extruder(2));
 		
-		factory.InitializeScheduler(new MockScheduler());
+		factory.InitializeScheduler(new Scheduler());
 		factory.InitializeReporter(new Reporter());
 		
 		IComponentTimer timer = new ComponentTimer(33300); //9.25 hours
---
+++ SonocoScheduler/src/Implementation/Scheduler.java
@@ -0,0 +1,58 @@
+package Implementation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Implementation.InstanceFactory;
+import interfaces.IPress;
+import interfaces.IScheduler;
+import interfaces.ITimedBasedComponent;
+
+public class Scheduler implements IScheduler {
+	private int _totalRemainingMolds;
+	@Override
+	public IPress getNextPress() {
+		List<IPress> availablePresses = new ArrayList<IPress>();
+		for(IPress press : InstanceFactory.get().GetAllPresses()) {
+			if(press instanceof ITimedBasedComponent) {
+				if(((ITimedBasedComponent) press).isActive() == false) {
+					availablePresses.add(press);
+				}
+			}
+		}
+		
+		_totalRemainingMolds = this.moldsRemaining(availablePresses);
+		int index = -1;
+		for(IPress press : availablePresses) {
+			if (index < 0 && !availablePresses.isEmpty()) {
+				index = 0;
+			}
+			else if (weightedScore(press) < weightedScore(availablePresses.get(index))){
+				index = availablePresses.indexOf(press);
+			}
+		}
+		
+		if(availablePresses.isEmpty() == false) {
+			return availablePresses.get(index);
+		}
+		
+		return null;
+	}
+	
+	private int weightedScore(IPress press) {
+		if (press.getRemaningMolds() > 0) {
+			return press.getScore() * (1 - (press.getRemaningMolds()/ _totalRemainingMolds));
+		}
+		else {
+			return press.getScore();
+		}
+	}
+	
+	private int moldsRemaining(List<IPress> availablePresses) {
+		int moldsRemaining = 0;
+		for(IPress press : availablePresses) {
+			moldsRemaining += press.getRemaningMolds();
+		}
+		return moldsRemaining;
+	}
+}
