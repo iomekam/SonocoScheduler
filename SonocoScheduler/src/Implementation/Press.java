fd46cf7b9a9e0f0adf58a5ce6e68fcd6c7c70cc5
rleonard3 <duce332@yahoo.com>
2016-08-21 21:18:39 -0400

    -Adding Implementation package and press class

---
+++ SonocoScheduler/src/Implementation/Press.java
@@ -0,0 +1,50 @@
+package Implementation;
+
+import interfaces.IPress;
+import interfaces.ITimedBasedComponent;
+
+public class Press implements IPress, ITimedBasedComponent {
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void getTimeRemaining() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isActive() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getScore() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getDistance() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getLimit() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getTotalMoldsCreated() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
